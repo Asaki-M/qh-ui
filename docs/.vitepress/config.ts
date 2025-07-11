b83e12daa42902db32d723fd1e9e3f854a58094f
Qinghuan <1041220793@qq.com>
2025-07-11 16:29:04 +0800

    feat: update components docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,14 +1,15 @@
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Qinghuan UI components",
-  description: "A UI components library base on reka ui",
+  title: 'Qinghuan UI components',
+  description: 'A UI components library base on reka ui',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Examples', link: '/markdown-examples' },
     ],
 
     sidebar: [
@@ -16,13 +17,16 @@ export default defineConfig({
         text: 'Examples',
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+      { icon: 'github', link: 'https://github.com/Asaki-M/qh-ui' },
+    ],
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
---
+++ docs/.vitepress/demo/Avatar.demo.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { Avatar } from 'qh-ui'
+import 'qh-ui/index.css'
+</script>
+
+<template>
+  <div class="text-2xl font-bold">
+    Qh UI Avatar Demo
+  </div>
+  <div class="space-y-4">
+    <div class="space-y-2">
+      <h2 class="text-lg font-semibold">
+        Different Sizes
+      </h2>
+      <div class="flex items-center space-x-2">
+        <Avatar size="xs" />
+        <Avatar size="sm" />
+        <Avatar size="md" />
+        <Avatar size="lg" />
+        <Avatar size="xl" />
+        <Avatar size="2xl" />
+        <Avatar size="3xl" />
+      </div>
+    </div>
+
+    <div class="space-y-2">
+      <h2 class="text-lg font-semibold">
+        With Image
+      </h2>
+      <Avatar
+        size="xl"
+        src="https://vitepress.dev/vitepress-logo-mini.svg"
+        fallback="VP"
+      />
+    </div>
+
+    <div class="space-y-2">
+      <h2 class="text-lg font-semibold">
+        With Fallback Text
+      </h2>
+      <div class="flex items-center space-x-2">
+        <Avatar
+          size="md"
+          fallback="A"
+          radius="full"
+        />
+        <Avatar
+          size="lg"
+          fallback="Test"
+          radius="md"
+        />
+        <Avatar
+          size="xl"
+          fallback="UI"
+          radius="lg"
+        />
+      </div>
+    </div>
+  </div>
+</template>
